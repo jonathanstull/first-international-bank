c4724f8f34ccd0762189c1ba300f01e79b4a9ee1
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-23 09:20:07 -0700

    add service logic fetching ExchangeRate API

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -1,1 +1,16 @@
 // template API call with key removed https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD
+
+export default class CurrencyConversion {
+  static getLatestConversionRates() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if(!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+  }
+}

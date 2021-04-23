c546c2355a4014edb7d60a00a57e0a9ac289977c
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-23 12:06:12 -0700

    WIP: throw error for undefined origin currency

--- src/main.js
+++ src/main.js
@@ -10,9 +10,10 @@ function showRates(response) {
     currencyArray.forEach(function(countryCurrency) {
       $('#currency-origin').append(`<option value="${countryCurrency[1]}">${countryCurrency[0]}</option>`);
       $('#currency-target').append(`<option value="${countryCurrency[1]}">${countryCurrency[0]}</option>`);
+      return currencyArray;
     });
   } else {
-    $('#response-error').html(`<p>There was an error processing your request: ${response["error-type"]}</p>`);
+    $('#error').html(`<p>There was an error processing your request: ${response["error-type"]}</p>`);
   }
 }
 
@@ -20,24 +21,33 @@ function convertCurrency(exchangeAmount, originRate, targetRate) {
   return (exchangeAmount / originRate) * targetRate;
 }
 
-function showConversion(originRate, targetRate, convertedAmount) {
-  let originCurrency = $('#currency-origin option:selected').text();
+function showConversion(originCurrency, originRate, targetRate, convertedAmount) {
   let targetCurrency = $('#currency-target option:selected').text();
   let exchangeRate = (1 / originRate) * targetRate;
   $('#converted-currency').html(`<h3>${targetCurrency}${convertedAmount}</h3>`);
   $('#conversion-information').append(`<p>The going exchange rate from ${originCurrency} to ${targetCurrency} is ${exchangeRate}.</p>`);
 }
 
+function throwConversionError(originCurrency) {
+  // currencyArray.forEach(function(currency) {
+  //   if (currency[originCurrency] === undefined) {
+  //     $('#error').html(`<p>The currency you selected does not exist or cannot be converted with the exchange rates available."</p>`);
+  //   }
+  // });
+}
+
 $(document).ready(function() {
   CurrencyConversion.getLatestConversionRates()
-  .then(function(response) {
-    showRates(response);
+    .then(function(response) {
+      showRates(response);
   });
   $('#convert-btn').click(function() {
+    let originCurrency = $('#currency-origin option:selected').text();
     let exchangeAmount = $('#currency-input').val();
     let originRate = $('#currency-origin').val();
     let targetRate = $('#currency-target').val();
     let convertedAmount = convertCurrency(exchangeAmount, originRate, targetRate);
-    showConversion(originRate, targetRate, convertedAmount);
+    throwConversionError(originCurrency);
+    showConversion(originCurrency, originRate, targetRate, convertedAmount);
   });
 });

761c32dbbfd1bdb3443c525a69a4f1c35f03ddcb
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-23 14:58:02 -0700

    add throwConversionError to handle edge case in which currency is not available

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -9,7 +9,19 @@ export default class CurrencyConversion {
       })
       .catch(function(error) {
         return error;
-      })
+      });
+  }
+
+  static async getLatestConversionRatesAgain() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyConversion from './js/exchange-service.js';
 
-function showRates(response) {
+
+function showCurrencies(response) {
+  let currencyArray = Object.entries(response.conversion_rates);
   if (response.conversion_rates) {
-    let currencyArray = Object.entries(response.conversion_rates);
     currencyArray.forEach(function(countryCurrency) {
       $('#currency-origin').append(`<option value="${countryCurrency[1]}">${countryCurrency[0]}</option>`);
       $('#currency-target').append(`<option value="${countryCurrency[1]}">${countryCurrency[0]}</option>`);
-      return currencyArray;
     });
   } else {
     $('#error').html(`<p>There was an error processing your request: ${response["error-type"]}</p>`);
@@ -28,26 +28,35 @@ function showConversion(originCurrency, originRate, targetRate, convertedAmount)
   $('#conversion-information').append(`<p>The going exchange rate from ${originCurrency} to ${targetCurrency} is ${exchangeRate}.</p>`);
 }
 
-function throwConversionError(originCurrency) {
-  // currencyArray.forEach(function(currency) {
-  //   if (currency[originCurrency] === undefined) {
-  //     $('#error').html(`<p>The currency you selected does not exist or cannot be converted with the exchange rates available."</p>`);
-  //   }
-  // });
+// async function callApi() {
+//   const response = await CurrencyConversion.getLatestConversionRatesAgain();
+//   return response;
+// }
+
+function throwConversionError(response) {
+  let currencyArray = Object.keys(response.conversion_rates);
+  let originCurrency = $('#currency-origin option:selected').text();
+  if (currencyArray.includes(originCurrency) === false) {
+    $('#error').html(`<p>The requested currency either does not exist or the exchange rate is not available.</p>`);
+  }
 }
+
 
 $(document).ready(function() {
   CurrencyConversion.getLatestConversionRates()
     .then(function(response) {
-      showRates(response);
-  });
+      showCurrencies(response);
+    });
   $('#convert-btn').click(function() {
     let originCurrency = $('#currency-origin option:selected').text();
     let exchangeAmount = $('#currency-input').val();
     let originRate = $('#currency-origin').val();
     let targetRate = $('#currency-target').val();
     let convertedAmount = convertCurrency(exchangeAmount, originRate, targetRate);
-    throwConversionError(originCurrency);
+    CurrencyConversion.getLatestConversionRates()
+      .then(function(response) {
+        throwConversionError(response);
+      });
     showConversion(originCurrency, originRate, targetRate, convertedAmount);
   });
 });

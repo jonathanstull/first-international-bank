c16fc888a5f0096096eaf5ecba938d9aac24ab0f
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-23 11:02:57 -0700

    add function convertCurrency successfully converting any currency into another

--- src/main.js
+++ src/main.js
@@ -7,13 +7,24 @@ import CurrencyConversion from './js/exchange-service.js';
 function showRates(response) {
   console.log(response);
   if (response.conversion_rates) {
-    let currencyArray = Object.entries(response.conversion_rates)
+    let currencyArray = Object.entries(response.conversion_rates);
     currencyArray.forEach(function(countryCurrency) {
-      $('#currency-code').append(`<option value="${countryCurrency[1]}">${countryCurrency[0]}</option>`);
-    })
+      console.log(currencyArray);
+      $('#currency-origin').append(`<option value="${countryCurrency[1]}">${countryCurrency[0]}</option>`);
+      $('#currency-target').append(`<option value="${countryCurrency[1]}">${countryCurrency[0]}</option>`);
+    });
   } else {
     $('#response-error').html(`<p>There was an error processing your request: ${response["error-type"]}</p>`);
   }
+}
+
+function convertCurrency(exchangeAmount, originRate, targetRate) {
+  return (exchangeAmount / originRate) * targetRate;
+  // let convertedAmount;
+  // // convert to USD
+  // let amountInUsd = exchangeAmount / originRate;
+  // // convert to foreign currency
+  // convertedAmount = amountInUsd * targetRate;
 }
 
 $(document).ready(function() {
@@ -22,5 +33,10 @@ $(document).ready(function() {
     showRates(response);
   });
   $('#convert-btn').click(function() {
+    let exchangeAmount = $('#currency-input').val();
+    let originRate = $('#currency-origin').val();
+    let targetRate = $('#currency-target').val();
+    let convertedAmount = convertCurrency(exchangeAmount, originRate, targetRate);
+    console.log(convertedAmount);
   });
 });
